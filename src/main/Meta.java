c2834d0621926cd2a1ae78ec5262dac98a6cc447
GodNoden <noe2000@live.com.mx>
2022-06-06 20:05:17 -0500

    push de 06/06/2022

--- src/main/Meta.java
+++ src/main/Meta.java
@@ -1,23 +1,12 @@
 package main;
 
 public class Meta extends Hallway {
-    
-    public Meta() {
-        super();
-    }
 
-    public Meta(Node back, Node front, int pos, int player) {
-        super(back, front, pos, player);
-    }
+  public Meta() {
+    super();
+  }
 
-    public Meta(Node node) {
-        super(node);
-    }
-
-    public Meta(Hallway hallway) {
-        super(hallway);
-    }
-    
-
-    
+  public Meta(Hallway back, int player) {
+    super(back, null, -2, player);
+  }
 }
--- src/main/PreHallway.java
+++ src/main/PreHallway.java
@@ -2,20 +2,16 @@ package main;
 
 public class PreHallway extends GrayZone {
 
-  private static Hallway Hallway;
-
   public PreHallway(GrayZone back, Hallway front, int player) { //la posicion del prehall siempre va a ser 0 porque es la cabeza
     super(back, front, 0, player);
-    if (back.passHall(player)) {}
+    //if (back.passHall(player)) {}
   }
 
-  public PreHallway(GrayZone back, int player) {
-    super(back, Hallway, 0, player);
+  public PreHallway(Node back, Hallway hallway, int player) {
+    super(back, hallway, 0, player);
   }
-
-
-  public PreHallway(Node node, Hallway hallway) {
-    super(node);
-    this.Hallway = hallway;
-}
+  // public PreHallway(Node node, Hallway hallway) {
+  //   super(node);
+  //   this.Hallway = hallway;
+  // }
 }
